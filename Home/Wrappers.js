ca165a48d8fc6597f0cf7fc6884a7b94d799e6a1
Benjamin Shafii <benjamin.shafii@gmail.com>
2017-09-30 15:56:32 +0200

    Update Home Layout

--- Home/Wrappers.js
+++ Home/Wrappers.js
@@ -2,13 +2,22 @@ import styled from 'styled-components';
 
 export const PostBox = styled.div`
   border: 2px solid #000;
-  width: 100px;
-  height: 100px;
+  min-width: 100%;
+  min-height: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
+  &:hover {
+    background-color: #bdbdbd;
+    > a {
+    color:white;
+    }
+  }
 `
 
 export const BoxContainer = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2em;
 `
