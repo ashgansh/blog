dc85cb1d371a04eca41e4612ce4e3926d8514d28
Benjamin Shafii <benjamin.shafii@gmail.com>
2017-09-30 16:06:21 +0200

    Different styling for Hero

--- Hero/index.js
+++ Hero/index.js
@@ -4,11 +4,13 @@ export default styled.div`
   padding: 1em;
 	line-height: 50px;
   min-width: 50%;
+  font-size: 0.9em;
   font-family: Courrier, "Courrier New";
-  color: #263238;
+  background-color: #eeeeee;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  border: 2px dashed;
+  border: 2px solid;
+  box-shadow: 3px 3px 3px grey
 `
 
